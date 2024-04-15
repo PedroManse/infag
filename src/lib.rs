7187d68ae1b441a3e0dbb2e7d8e2a5ee68265526
Pedro Manse <pedro.manse@dmk3.com.br>
2024-04-15 11:27:02 -0300

    poem-based http server (with htmx) for lev.-based searching

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,13 @@ pub enum LevError {
     YWordOutOfBound,
 }
 
-use std::cell::RefCell;
+use std::sync::Mutex;
 // wagner-fischer
 #[derive(Debug)]
 pub struct Lev {
     xlen: usize,
     ylen: usize,
-    matrix: RefCell<Vec<Vec<i32>>>,
+    matrix: Mutex<Vec<Vec<i32>>>,
 }
 
 impl Lev {
@@ -25,11 +25,11 @@ impl Lev {
         Lev {
             xlen,
             ylen,
-            matrix: RefCell::new(matrix),
+            matrix: Mutex::new(matrix),
         }
     }
 
-    fn neighbors(&self, bmatrix: &Vec<Vec<i32>>, y: usize, x: usize) -> i32 {
+    fn neighbors(&self, bmatrix: &[Vec<i32>], y: usize, x: usize) -> i32 {
         std::cmp::min(
             bmatrix[y - 1][x - 1],
             std::cmp::min(bmatrix[y][x - 1], bmatrix[y - 1][x]),
@@ -43,7 +43,7 @@ impl Lev {
         if yword.len() >= self.ylen {
             return Err(LevError::YWordOutOfBound);
         }
-        let mut matrix = self.matrix.borrow_mut();
+        let mut matrix = self.matrix.lock().unwrap();
         let mut ychars = yword.chars();
         for y in 0..self.ylen - 1 {
             let ychar = ychars.next();
@@ -68,13 +68,7 @@ pub struct Table {
 
 impl Table {
     pub fn new(max_search: usize, col_count: usize, content: Vec<Vec<String>>) -> Option<Table> {
-        if content
-            .iter()
-            .map(Vec::len)
-            .filter(|a| *a != col_count)
-            .next()
-            .is_some()
-        {
+        if content.iter().map(Vec::len).any(|a| a != col_count) {
             return None;
         }
         let max_yword = content
@@ -89,29 +83,50 @@ impl Table {
             lev_matrix: Lev::new(max_search, max_yword + 2),
         })
     }
-    pub fn compare(
-        &self,
-        search: &str,
-        take: usize,
-    ) -> Vec<(&[String], i32)> {
+    pub fn compare(&self, search: &str, take: usize) -> Vec<(&[String], i32)> {
         let mut a = self
             .content
             .iter()
             .map(|row| {
                 (
                     row.as_slice(),
-                    row
-                    .iter()
-                    .map(|cmp| self.lev_matrix.compare(search, cmp))
-                    .map(Result::ok)
-                    .flatten()
-                    .min()
-                    .unwrap_or(999)
+                    row.iter()
+                        .map(|cmp| self.lev_matrix.compare(search, cmp))
+                        .filter_map(Result::ok)
+                        .min()
+                        .unwrap_or(999),
                 )
             })
             .take(take)
             .collect::<Vec<_>>();
-        a.sort_by_key(|(_, lev)|*lev);
+        a.sort_by_key(|(_, lev)| *lev);
         a
+    }
+}
+
+pub struct Rows<'a> {
+    content: Vec<&'a [String]>,
+}
+
+impl<'a> From<Vec<(&'a [String], i32)>> for Rows<'a> {
+    fn from(content: Vec<(&'a [String], i32)>) -> Rows<'a> {
+        Rows {
+            content: content.into_iter().map(|(row, _)| row).collect(),
+        }
+    }
+}
+
+use maud::{html, Markup, Render, PreEscaped};
+impl Render for Rows<'_> {
+    fn render(&self) -> Markup {
+        html! {
+            @for row in &self.content {
+                tr {
+                    @for col in row.iter() {
+                        td { (PreEscaped(col)) }
+                    }
+                }
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,96 @@
-// search = xword
-// table = yword
+use maud::{Render, html, DOCTYPE};
+use poem::{
+    get, handler,
+    listener::TcpListener,
+    middleware::AddData,
+    web::{Data, Html, Query},
+    EndpointExt, Route, Server,
+};
+use serde::Deserialize;
+use std::sync::Arc;
 
-use infag::*;
-fn main() {
-    let table = fake_table();
-
-    let x: Vec<_> = table.compare("first", 2);
-    println!("{x:?}");
+#[derive(Deserialize)]
+struct TextSearch {
+    search: String,
+    take: Option<usize>,
 }
 
-fn fake_table() -> Table {
+#[handler]
+fn index() -> Html<String> {
+    Html(
+        html! { (DOCTYPE) html {
+            head {
+                script src="https://unpkg.com/htmx.org@1.9.11"{}
+                style { """
+table {
+	min-width: 90%;
+	margin-left: 5%;
+	margin-right: 5%;
+	margin-bottom: 5px;
+	border-collapse: collapse;
+	letter-spacing: 1px;
+	font-size: 0.8rem;
+
+	td {
+		text-align: left;
+		border-left: 1px solid #666;
+		border-right: 1px solid #666;
+		border-bottom: 1px solid #222;
+		padding: 2px;
+	}
+
+                """ }
+            }
+            body {
+                input
+                    type="text"
+                    hx-get="/query"
+                    hx-trigger="change"
+                    hx-swap="innerHTML"
+                    hx-target="#tbody"
+                    name="search"
+                    { }
+                table{
+                    thead {
+                        tr{
+                            th{"name"}
+                            th{"signature"}
+                        }
+                    }
+                    tbody id="tbody" {}
+                }
+            }
+        } }
+        .into(),
+    )
+}
+
+#[handler]
+fn search(
+    Query(TextSearch { search, take }): Query<TextSearch>,
+    state: Data<&Arc<Table>>,
+) -> Html<String> {
+    let take = take.unwrap_or(5);
+    let rows: Rows<'_> = state.compare(&search, take).into();
+    Html( rows.render().into() )
+}
+
+use infag::*;
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), std::io::Error> {
+    let state = fake_table().await;
+    let app = Route::new()
+        .at("/", get(index))
+        .at("/query", get(search))
+        .with(AddData::new(Arc::new(state)));
+
+    Server::new(TcpListener::bind("0.0.0.0:8000"))
+        .name("add-data")
+        .run(app)
+        .await
+}
+
+async fn fake_table() -> Table {
     let src_max = std::env::var("LEVSMAX")
         .map(|a| a.parse().ok())
         .ok()
@@ -19,10 +100,11 @@ fn fake_table() -> Table {
         src_max,
         2,
         vec![
-            vec!["search", "Vec<T>,T -> Option<T>"],
-            vec!["first", "Vec<T> -> Option<T>"],
-            vec!["take", "Option<T> -> Option<T>"],
-            vec!["ok_or", "Option<T>, E -> Eesult<T, E>"],
+            vec!["<a href=\"https://doc.rust-lang.org/std/vec/struct.Vec.html#method.pop\">pop</a>",  "Vec&lt;T&gt; -> Option&lt;T&gt;"],
+            vec!["<a href=\"https://example.com\">find</a>",  "Vec&lt;T&gt;,Fn(T)->bool -> Option&lt;T&gt;"],
+            vec!["<a href=\"https://example.com\">first</a>", "Vec&lt;T&gt; -> Option&lt;T&gt;"            ],
+            vec!["<a href=\"https://example.com\">take</a>",  "Option&lt;T&gt; -> Option&lt;T&gt;"         ],
+            vec!["<a href=\"https://example.com\">ok_or</a>", "Option&lt;T&gt;, E -> Result&lt;T, E&gt;"   ],
         ]
         .into_iter()
         .map(|fnc| fnc.into_iter().map(String::from).collect())
